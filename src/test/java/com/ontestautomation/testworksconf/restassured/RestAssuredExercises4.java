7763f5289f52fd95759de2fe00c6a930dc0d3ee8
Bas Dijkstra <bas@ontestautomation.com>
2016-09-26 08:29:29 +0200

    Added exercises and answers part 4

--- src/test/java/com/ontestautomation/testworksconf/restassured/RestAssuredExercises4.java
+++ src/test/java/com/ontestautomation/testworksconf/restassured/RestAssuredExercises4.java
@@ -3,32 +3,25 @@ package com.ontestautomation.testworksconf.restassured;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
-import java.util.concurrent.TimeUnit;
-
-import io.restassured.RestAssured;
-
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class RestAssuredExercises3 {
+public class RestAssuredExercises4 {
 	
-	@BeforeClass
-	public void initPath() {
-		
-		RestAssured.baseURI = "http://localhost:9876";
-	}
-		
 	/*******************************************************
-	 * Request an authentication token API and write the
-	 * response to the console. Use preemptive Basic authentication:
+	 * Request an authentication token for the Paypal sandbox
+	 * API and write the response to the console
+	 * Use Basic authentication
 	 * username = oauth
 	 * password = gimmeatoken
 	 * Use /v1/oauth2/token
+	 * Store this authentication token in a String variable
+	 * for future reference
 	 ******************************************************/
-	
+		
 	@BeforeClass
 	public void retrieveOAuthToken() {
-		
+						
 		given().
 		when().
 		then();
@@ -37,28 +30,13 @@ public class RestAssuredExercises3 {
 	/*******************************************************
 	 * Request a list of payments for this account and check
 	 * that the number of payments made equals 4.
-	 * Use OAuth2 authentication with the previously retrieved
+	 * Use OAuth2 authenticatie with the previously stored
 	 * authentication token.
 	 * Use /v1/payments/payment/
 	 ******************************************************/
 	
 	@Test
 	public void checkNumberOfPayments() {
-		
-		given().
-		when().
-		then();
-	}
-	
-	/*******************************************************
-	 * Request the list of all circuits that hosted a
-	 * Formula 1 race in 2014 and check that this request is
-	 * answered within 100 ms
-	 * Use /api/f1/2014/circuits.json
-	 ******************************************************/
-	
-	@Test
-	public void checkResponseTimeFor2014CircuitList() {
 		
 		given().
 		when().
